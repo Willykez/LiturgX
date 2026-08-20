8987f4a1debb4650d87fdc86d1d354249131b2f1
Willykez <willykez01@gmail.com>
2026-08-20 06:50:18 +0300

    Update ImageSaveShareButtons.kt

--- app/src/main/java/com/willykez/liturgx/ui/components/ImageSaveShareButtons.kt
+++ app/src/main/java/com/willykez/liturgx/ui/components/ImageSaveShareButtons.kt
@@ -9,6 +9,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
