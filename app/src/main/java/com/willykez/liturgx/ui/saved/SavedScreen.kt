ccd41cdb31dbc8440e6e5082506badc3eff98088
Willykez <willykez01@gmail.com>
2026-09-06 20:15:15 +0300

    Update SavedScreen.kt

--- app/src/main/java/com/willykez/liturgx/ui/saved/SavedScreen.kt
+++ app/src/main/java/com/willykez/liturgx/ui/saved/SavedScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -102,7 +103,7 @@ fun SavedScreen(
             "Yaliyohifadhiwa",
             style = MaterialTheme.typography.titleLarge,
             color = onBg,
-            modifier = Modifier.padding(horizontal = 20.dp, top = 4.dp, bottom = 4.dp)
+            modifier = Modifier.padding(horizontal = 20.dp, vertical = 4.dp)
         )
         Row(
             Modifier.fillMaxWidth().padding(horizontal = 20.dp, vertical = 12.dp),
